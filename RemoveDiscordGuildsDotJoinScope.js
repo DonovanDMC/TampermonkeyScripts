9067d6ce89c733ce2244347efff596db3b3a35b0
Donovan Daniels <hewwo@yiff.rocks>
2021-03-19 15:42:57 -0500

    Remove Discord guilds.join Scope

---
+++ RemoveDiscordGuildsDotJoinScope.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         Remove Discord guilds.join Scope
+// @namespace    https://discord.com
+// @version      1.0.0
+// @description  Remove the guilds.join scope from oauth urls.
+// @author       Donovan_DMC
+// @match        https://discord.com/oauth2/authorize*
+// @grant        none
+// @updateURL    https://raw.githubusercontent.com/DonovanDMC/TampermonkeyScripts/master/RemoveDiscordGuildsDotJoinScope.js
+// @downloadURL  https://raw.githubusercontent.com/DonovanDMC/TampermonkeyScripts/master/RemoveDiscordGuildsDotJoinScope.js
+// ==/UserScript==
+
+(function () {
+	'use strict';
+
+	const query = window.location.href.split("?")[1].split("&").map(v => ({ [v.split("=")[0]]: v.split("=")[1] })).reduce((a, b) => ({ ...a, ...b }), {});
+	const o = String(query.scope);
+	query.scope = decodeURIComponent(query.scope).split(" ").filter(j => j !== "guilds.join").join(query.scope.indexOf("+") !== -1 ? "+" : "%20");
+	if (o !== query.scope) window.location.href = `${window.location.href.split("?")[0]}?${Object.entries(query).map(([a, b]) => `${a}=${b}`).join("&")}`;
+})();
