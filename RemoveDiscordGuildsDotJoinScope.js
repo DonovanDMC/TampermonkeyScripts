fd72ec332dbc6bd0a035a7b838cfab95bb3146b8
Donovan Daniels <hewwo@yiff.rocks>
2021-03-19 15:44:00 -0500

    add icon to rm guilds.join

--- RemoveDiscordGuildsDotJoinScope.js
+++ RemoveDiscordGuildsDotJoinScope.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name         Remove Discord guilds.join Scope
 // @namespace    https://discord.com
-// @version      1.0.0
+// @version      1.0.1
 // @description  Remove the guilds.join scope from oauth urls.
 // @author       Donovan_DMC
 // @match        https://discord.com/oauth2/authorize*
+// @icon         https://www.google.com/s2/favicons?domain=discord.com
 // @grant        none
 // @updateURL    https://raw.githubusercontent.com/DonovanDMC/TampermonkeyScripts/master/RemoveDiscordGuildsDotJoinScope.js
 // @downloadURL  https://raw.githubusercontent.com/DonovanDMC/TampermonkeyScripts/master/RemoveDiscordGuildsDotJoinScope.js
